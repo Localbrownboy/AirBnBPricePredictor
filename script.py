6801a65b3191368762e28659a3e401a80653f8fa
Nima Seifi <nseifihe@sfu.ca>
2024-11-23 18:10:32 -0800

    added a data set to eda script

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ print(f"\nYou are in Data Preprocessing:\n")
 os.system('python3 ./1.PreProcessData.py ./data/listings.csv')
 
 print(f"\nYou are in EDA:\n")
-os.system('python3 ./2.EDA.py ./data/intermediate_listings_encoded.csv ./data/intermediate_listings_encoded_and_scaled.csv')
+os.system('python3 ./2.EDA.py ./data/intermediate_listings_encoded.csv ./data/intermediate_listings_encoded_and_scaled.csv ./data/processed_listings.csv')
 
 print(f"\nYou are in Clustering:\n")
 os.system('python3 ./3.Clustering.py ./data/processed_listings.csv')
